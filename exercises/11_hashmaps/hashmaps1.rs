06acd9507e775f06264bb5bee4be48f0da443036
sumeyacodes <sumeya4hmed@gmail.com>
2025-07-19 01:24:09 +0100

    completed: exercise 11

--- exercises/11_hashmaps/hashmaps1.rs
+++ exercises/11_hashmaps/hashmaps1.rs
@@ -8,18 +8,27 @@ use std::collections::HashMap;
 
 fn fruit_basket() -> HashMap<String, u32> {
     // TODO: Declare the hash map.
-    // let mut basket =
+    let mut basket = HashMap::new();
 
     // Two bananas are already given for you :)
     basket.insert(String::from("banana"), 2);
 
     // TODO: Put more fruits in your basket.
 
+    basket.insert(String::from("apples"), 2);
+
+    basket.insert(String::from("mango"), 2);
+
+    println!("{:#?}", basket);
+
     basket
 }
 
 fn main() {
     // You can optionally experiment here.
+
+    fruit_basket();
+    
 }
 
 #[cfg(test)]
--- exercises/11_hashmaps/hashmaps2.rs
+++ exercises/11_hashmaps/hashmaps2.rs
@@ -32,11 +32,25 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         // TODO: Insert new fruits if they are not already present in the
         // basket. Note that you are not allowed to put any type of fruit that's
         // already present!
+
+        basket.entry(fruit).or_insert(1);
+
+        println!("{:#?}", basket); 
+
     }
 }
 
 fn main() {
     // You can optionally experiment here.
+
+    fn get_fruit_basket() -> HashMap<Fruit, u32> {
+        let content = [(Fruit::Apple, 4), (Fruit::Mango, 2), (Fruit::Lychee, 5)];
+        HashMap::from_iter(content)
+    }
+
+    let mut basket = get_fruit_basket();
+    
+    fruit_basket(&mut basket);
 }
 
 #[cfg(test)]
